e5284d7ea5f22b495a0c2420e45b142f2694722a
Colin Williams <github@crwilliams.co.uk>
2011-03-12 11:13:59 +0000

    Added caching
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@11 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/resources/clustericon.php
+++ www/htdocs/resources/clustericon.php
@@ -1,54 +1,59 @@
 <?php
-$imgs = $_GET['i'];
 function simagecreatefrompng($url) {
 	if(preg_match("/^https?:\/\//", $url) || !preg_match("/\.\./", $url))
 	{
 		return imagecreatefrompng($url);
 	}
 }
-if(array_key_exists('base', $_GET))
-	$rimg = simagecreatefrompng($_GET['base']);
-else
-	$rimg = simagecreatefrompng('blackness.png');
-$img1 = simagecreatefrompng($imgs[0]);
-$i = count($imgs);
-if($i >= 2)
+$hash = md5($_SERVER['QUERY_STRING']);
+$filename = 'cache/'.$hash.'.png';
+if(!file_exists($filename))
 {
-	$img2 = simagecreatefrompng($imgs[1]);
-}
-if($i >= 3)
-{
-	$img3 = simagecreatefrompng($imgs[2]);
-}
-if($i >= 4)
-{
-	$img4 = simagecreatefrompng($imgs[3]);
-}
-if($i >= 4)
-{
-	imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img2, 16, 6, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img3, 6, 16, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img4, 16, 16, 5, 5, 10, 10, 22, 22);
-}
-if($i == 3)
-{
-	imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img2, 16, 6, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img3, 11, 16, 5, 5, 10, 10, 22, 22);
-}
-if($i == 2)
-{
-	imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
-	imagecopyresampled($rimg, $img2, 16, 16, 5, 5, 10, 10, 22, 22);
-}
-if($i == 1)
-{
-	header('Content-type: image/png');
-	imagesavealpha($img1,true);
-	imagepng($img1);
+	$imgs = $_GET['i'];
+	if(array_key_exists('base', $_GET))
+		$rimg = simagecreatefrompng($_GET['base']);
+	else
+		$rimg = simagecreatefrompng('blackness.png');
+	$img1 = simagecreatefrompng($imgs[0]);
+	$i = count($imgs);
+	if($i >= 2)
+	{
+		$img2 = simagecreatefrompng($imgs[1]);
+	}
+	if($i >= 3)
+	{
+		$img3 = simagecreatefrompng($imgs[2]);
+	}
+	if($i >= 4)
+	{
+		$img4 = simagecreatefrompng($imgs[3]);
+	}
+	if($i >= 4)
+	{
+		imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img2, 16, 6, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img3, 6, 16, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img4, 16, 16, 5, 5, 10, 10, 22, 22);
+	}
+	if($i == 3)
+	{
+		imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img2, 16, 6, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img3, 11, 16, 5, 5, 10, 10, 22, 22);
+	}
+	if($i == 2)
+	{
+		imagecopyresampled($rimg, $img1, 6, 6, 5, 5, 10, 10, 22, 22);
+		imagecopyresampled($rimg, $img2, 16, 16, 5, 5, 10, 10, 22, 22);
+	}
+	if($i == 1)
+	{
+		imagesavealpha($img1,true);
+		imagepng($img1, $filename);
+	}
+	imagesavealpha($rimg,true);
+	imagepng($rimg, $filename);
 }
 header('Content-type: image/png');
-imagesavealpha($rimg,true);
-imagepng($rimg);
+fpassthru(fopen($filename, 'r'));
 ?>
