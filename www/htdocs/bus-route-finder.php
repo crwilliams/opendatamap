dee18dabf6641caca8db89b9e781c3028a89bf75
Colin Williams <github@crwilliams.co.uk>
2011-03-30 18:21:46 +0000

    Added bus-route-finder
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@70 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

---
+++ www/htdocs/bus-route-finder.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<?php
+error_reporting(0);
+if(!$include && substr($_SERVER['REQUEST_URI'], -4, 4) == '.php')
+	header('Location: bus-route-finder');
+include 'inc/options.php';
+?>
+<html>
+	<head>
+		<title>Find a bus route between campuses</title>
+		<!--<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />-->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+		<meta name="keywords" content="University of Southampton,map,Southampton,amenity,bus stop,building,site,campus" />
+		<meta name="description" content="Map of the University of Southampton, generated from Linked Open Data" />
+		<link rel="apple-touch-icon" href="img/opendatamap.png" />
+		<link rel="apple-touch-icon-precomposed" href="img/opendatamap.png" />
+		<link rel="shortcut icon" href="img/opendatamap.png" />
+		<script src="http://www.google.com/jsapi"></script>
+		<script type="text/javascript" src="js/jquery-1.5.min.js"></script>
+		<script type="text/javascript" src="js/m.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/reset.css" type="text/css">
+		<link rel="stylesheet" href="css/m.css" type="text/css">
+		<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-20609696-4']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+		</script>
+	</head>
+	<body>
+		<form onsubmit='window.location.href="?from="+$("#from").get(0).value+"&to="+$("#to").get(0).value; return false;'>
+			<div style='border:solid 3px gray; margin:10px; padding:10px;'>
+			I'd like to travel from...<br/>
+			<select name='from' id='from' onchange='selectSite(this.options[selectedIndex].value)'>
+				<?php getPlaces($_GET['from']); ?>
+			</select><br/>
+			to...<br/>
+			<select name='to' id='to' onchange='selectSite(this.options[selectedIndex].value)'>
+				<?php getPlaces($_GET['to']); ?>
+			</select><br/>
+			</div>
+			<div style='border:solid 3px gray; margin:10px; padding:10px;'>
+				<input type='submit' value='Show me' />
+			</div>
+		<form>
+		<div style='border:solid 3px gray; margin:10px; padding:10px;'>
+<?php
+if(isset($_GET['from']) && isset($_GET['to']))
+{
+	getRoutes($_GET['from'], $_GET['to']);
+}
+?>
+		</div>
+		<div id="credits"><?php $include = true; include 'credits.php' ?></div>
+	</body>
+</html>
--- www/htdocs/inc/options.php
+++ www/htdocs/inc/options.php
@@ -78,7 +78,7 @@ function getSpecificOfferings() {
 	}
 }
 
-function getSites() {
+function getSites($selected = null) {
 	global $endpoint;
 	$sites = sparql_get($endpoint, "
 		SELECT DISTINCT ?uri ?name WHERE {
@@ -88,7 +88,109 @@ function getSites() {
 	");
 	echo '<option value="">Select site</option>';
 	foreach($sites as $site) {
-		echo '<option value="'.$site['uri'].'">'.$site['name'].'</option>';
+		if($site['uri'] == $selected)
+			echo '<option value="'.$site['uri'].'" selected="true">'.$site['name'].'</option>';
+		else
+			echo '<option value="'.$site['uri'].'">'.$site['name'].'</option>';
 	}
 }
+
+function getPlaces($selected = null) {
+	global $endpoint;
+	$sites = sparql_get($endpoint, "
+		PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+		
+		SELECT DISTINCT ?uri ?name WHERE {
+		  ?uri <http://www.w3.org/2000/01/rdf-schema#label> ?name .
+		  ?stop foaf:based_near ?uri .
+		} ORDER BY ?name
+	");
+	echo '<option value="">Select place</option>';
+	foreach($sites as $site) {
+		if($site['uri'] == $selected)
+			echo '<option value="'.$site['uri'].'" selected="true">'.$site['name'].'</option>';
+		else
+			echo '<option value="'.$site['uri'].'">'.$site['name'].'</option>';
+	}
+}
+
+function getRoutes($from, $to) {
+	global $endpoint;
+	$routes = sparql_get($endpoint, "
+PREFIX soton: <http://id.southampton.ac.uk/ns/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX naptan: <http://transport.data.gov.uk/def/naptan/>
+
+SELECT DISTINCT ?sstoplabel ?dstoplabel ?routecode ?routelabel ?sstoprouteseq ?dstoprouteseq WHERE {
+  ?route skos:notation ?routecode .
+  ?route rdfs:label ?routelabel .
+  ?sstoproute soton:inBusRoute ?route .
+  ?dstoproute soton:inBusRoute ?route .
+  ?sstoproute soton:busRouteSequenceNumber ?sstoprouteseq .
+  ?dstoproute soton:busRouteSequenceNumber ?dstoprouteseq .
+  ?sstoproute soton:busStoppingAt ?sstop .
+  ?dstoproute soton:busStoppingAt ?dstop .
+  ?sstop a naptan:BusStop .
+  ?sstop foaf:based_near <$from> .
+  ?sstop rdfs:label ?sstoplabel .
+  ?dstop a naptan:BusStop .
+  ?dstop foaf:based_near <$to> .
+  ?dstop rdfs:label ?dstoplabel .
+  FILTER ( ?dstoprouteseq > ?sstoprouteseq )
+} ORDER BY ?routecode (?dstoprouteseq - ?sstoprouteseq)
+	");
+	foreach($routes as $route)
+	{
+		$code = $route['routecode'];
+		if($code != $oldcode) {
+			$oldstopcount = 0;
+			$oldcode = $code;
+		}
+		$stopcount = $route['dstoprouteseq'] - $route['sstoprouteseq'];
+		if($oldstopcount == 0 || $oldstopcount + 1 == $stopcount) {
+			$stops[$code]['from'][] = $route['sstoplabel'];
+			$stops[$code]['to'][] = $route['dstoplabel'];
+			if($oldstopcount == 0) {
+				$shortest[$code] = $stopcount;
+			}
+			$oldstopcount = $stopcount;
+		}
+
+/*
+		echo $route['sstoplabel'];
+		echo $route['dstoplabel'];
+		echo $route['routecode'];
+		echo $route['routelabel'];
+		echo $route['sstoprouteseq'];
+		echo $route['dstoprouteseq'];
+*/
+	}
+
+	asort($shortest);
+	echo "<table>";
+	echo "<tr><th>Bus Route</th><th>From</th><th>To</th></tr>";
+	foreach(array_keys($shortest) as $code) {
+		echo "<tr>";
+		echo "<td>$code</td>";
+		echo "<td><ul>";
+		$stops[$code]['from'] = array_reverse(array_unique($stops[$code]['from']));
+		foreach($stops[$code]['from'] as $from)
+		{
+			echo "<li>$from</li>";
+		}
+		echo "</ul></td>";
+		echo "<td><ul>";
+		$stops[$code]['to'] = array_unique($stops[$code]['to']);
+		foreach($stops[$code]['to'] as $to)
+		{
+			echo "<li>$to</li>";
+		}
+		echo "</ul></td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+
 ?>
