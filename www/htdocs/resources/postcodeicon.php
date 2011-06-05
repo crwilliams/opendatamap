99455548337ff993b9dd91f4ea5b340e620e7c47
Colin Williams <github@crwilliams.co.uk>
2011-06-05 00:10:59 +0000

    Updated icon colours and added postcode icon generator
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@110 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

---
+++ www/htdocs/resources/postcodeicon.php
@@ -0,0 +1,16 @@
+<?php
+$hash = md5($_SERVER['QUERY_STRING']);
+$filename = 'cache/pc_'.$hash.'.png';
+if(!file_exists($filename))
+{
+	$pc = explode(' ', $_GET['pc']);
+	$oimg = imagecreatefrompng("../img/icon/blank_a9a9a9.png");
+	$white = imagecolorallocate($oimg, 255, 255, 255);
+	imagestring($oimg, 2, 3, 3, $pc[0], $white);
+	imagestring($oimg, 2, 13, 14, $pc[1], $white);
+	imagesavealpha($oimg,true);
+	imagepng($oimg, $filename);
+}
+header('Content-type: image/png');
+fpassthru(fopen($filename, 'r'));
+?>
