0c53aabc261ddaeff7beaa2b2dd3f4f98a545683
Colin Williams <github@crwilliams.co.uk>
2011-06-14 23:33:42 +0000

    More restructuring.  Added sqldemo data source.
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@136 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/config.php
+++ www/htdocs/config.php
@@ -1,9 +1,14 @@
 <?
-$config['Site title'] = "University of Southampton Linked Open Data Map";
-$config['Site keywords'] = "University of Southampton,map,Southampton,amenity,bus stop,building,site,campus,interactive";
-$config['Site description'] = "Interactive Map of the University of Southampton, generated from Linked Open Data";
+if(file_exists('config.local.php'))
+{
+	include 'config.local.php';
+	return;
+}
+$config['Site title'] = "Map";
+$config['Site keywords'] = "map,interactive";
+$config['Site description'] = "Interactive Map";
 $config['default lat'] = 50.9355;
 $config['default long'] = -1.39595;
 $config['default zoom'] = 17;
-$config['datasource'] = 'sparql';
+$config['datasource'] = 'sqldemo';
 ?>
