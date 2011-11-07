0c1ad26cdc919e7a3f32612c34873bb013005fe2
Colin Williams <github@crwilliams.co.uk>
2011-11-07 00:21:40 +0000

    Added HTML version
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@271 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/alldata.php
+++ www/htdocs/alldata.php
@@ -2,25 +2,52 @@
 error_reporting(0);
 include_once "config.php";
 
-header('Content-Type: application/json');
+$html = false;
+if(isset($_GET['html']))
+	$html = true;
+
+if(!$html)
+	header('Content-Type: application/json');
 
 // This script should return info on all of the markers that can be visible on the map.
 
 $points = getAllDataPoints();
 
 //Begin response
-echo "[";
-//Each entry in the response is an array containing the following data:
-// * ID of marker
-// * Latitude of marker
-// * Longitude of marker
-// * Label of marker
-// * URL to marker icon
-foreach($points as $point) {
-	echo '["'.$point['id'].'",'.$point['lat'].','.$point['long'].',"'.$point['label'].'","'.$point['icon'].'"],';
+if(!$html)
+{
+	echo "[";
+	//Each entry in the response is an array containing the following data:
+	// * ID of marker
+	// * Latitude of marker
+	// * Longitude of marker
+	// * Label of marker
+	// * URL to marker icon
+	foreach($points as $point) {
+		echo '["'.$point['id'].'",'.$point['lat'].','.$point['long'].',"'.$point['label'].'","'.$point['icon'].'"],';
+	}
+	//End response (including empty element for convenience (required))
+	echo "[]]";
 }
-//End response (including empty element for convenience (required))
-echo "[]]";
+else
+{
+	foreach($points as $point) {
+		$d[$point['icon']][] = $point;
+	}
+	ksort($d);
+	foreach($d as $icon => $points)
+	{
+		echo '<img src="'.$points[0]['icon'].'" />';
+		echo '<ul>';
+		foreach($points as $point)
+		{
+			$cid = $point['id'];
+			$cicon = $point['icon'];
+			echo '<li><input type="checkbox" id="'.$cid.'" name="'.$cid.'" class="'.$cicon.'" /><label for="'.$cid.'">'.str_replace('\\', '', $point['label']).' (<a href="'.$point['id'].'">link</a>)</label></li>';
+		}
+		echo '</ul>';
+	}
+}
 
 function getAllDataPoints()
 {
