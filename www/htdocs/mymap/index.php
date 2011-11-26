f452d41eca854e84d033579d836225f20ecc80be
Colin Williams <github@crwilliams.co.uk>
2011-11-26 22:06:58 +0000

    Added user map list
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@290 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/mymap/index.php
+++ www/htdocs/mymap/index.php
@@ -1,25 +1,57 @@
 <?php
 include 'functions.inc.php';
 require_once('/home/opendatamap/mysql.inc.php');
-outputHeader("Map list for USERNAME", "", "GENERIC", true, true);
-?>
-	<h3>Your maps</h3>
-<?php
-$params[] = mysql_real_escape_string($_SESSION['username']);
+$username = "";
+$editmode = false;
+$header = false;
+if(isset($_GET['username']))
+{
+	$username = $_GET['username'];
+	outputHeader("Map list for ".$username, "", "GENERIC", true, false);
+	$header = true;
+	if(isset($_SESSION['username']) && $username == $_SESSION['username'])
+	{
+		$editmode = true;
+	}
+}
+else
+{
+	$editmode = true;
+}
+
+if($editmode && !$header)
+{
+	outputHeader("Map list for USERNAME", "", "GENERIC", true, true);
+}
+if($editmode)
+{
+	echo '<h3>Your maps</h3>';
+	$username = $_SESSION['username'];
+}
+else
+{
+	echo '<h3>'.$username.'&apos;s maps</h3>';
+}
+
+$params[] = mysql_real_escape_string($username);
 $q = 'SELECT mapid, name FROM maps WHERE username = \''.$params[0].'\' order by `name`';
 $res = mysql_query($q);
 echo '<ul>';
 while($row = mysql_fetch_assoc($res))
 {
 	echo '<li>'.$row['name'];
-	echo ' <a href=\''.$_SESSION['username'].'/'.$row['mapid'].'\'>(View RDF)</a>';
-	echo ' <a href=\''.$_SESSION['username'].'/'.$row['mapid'].'/edit\'>(Edit)</a>';
+	echo ' <a href=\''.$username.'/'.$row['mapid'].'\'>(View RDF)</a>';
+	if($editmode)
+		echo ' <a href=\''.$username.'/'.$row['mapid'].'/edit\'>(Edit)</a>';
 	echo '</li>';
 }
 echo '</ul>';
+if($editmode)
+{
 ?>
 	<a href='new'>Add map</a>
 <?php
+}
 
 outputFooter();
 ?>
