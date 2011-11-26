e6f63c2642de458249d4da997b99beb4f9a5e9eb
Colin Williams <github@crwilliams.co.uk>
2011-11-26 22:03:24 +0000

    Added email field
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@288 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/mymap/register.php
+++ www/htdocs/mymap/register.php
@@ -12,6 +12,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 		$errors[] = 'Username not set';
 		$bad['username'] = true;
 	}
+	if(trim($_POST['email']) == "")
+	{
+		$errors[] = 'Email address not set';
+		$bad['email'] = true;
+	}
 	if(trim($_POST['password1']) == "")
 	{
 		$errors[] = 'Password not set';
@@ -22,9 +27,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 		$errors[] = 'Passwords do not match';
 		$bad['password2'] = true;
 	}
-	foreach($errors as $error)
+	if(count($errors) > 0)
 	{
-		echo $error.'<br />';
+		echo '<div style="background-color:#FF9999; margin:10px; padding:10px; text-align:center">';
+		foreach($errors as $error)
+		{
+			echo $error.'<br />';
+		}
+		echo '</div>';
 	}
 }
 if(!$post || count($errors) > 0)
@@ -33,6 +43,7 @@ if(!$post || count($errors) > 0)
 <form action='register' method='post'>
 	<table>
 		<tr class='comp <?= $post && $bad['username'] ? "bad" : "" ?>'><td><label for='username'>Username:</label></td><td><input id='username' name='username' style='width:10em' value='<?= $_POST['username'] ?>' /></td></tr>
+		<tr class='comp <?= $post && $bad['email'] ? "bad" : "" ?>'><td><label for='email'>Email Address:</label></td><td><input id='email' name='email' style='width:20em' value='<?= $_POST['email'] ?>' /></td></tr>
 		<tr><td colspan='2'><hr /></td></tr>
 		<tr class='comp <?= $post && $bad['password1'] ? "bad" : "" ?>'><td><label for='password1'>Password:</label></td><td><input type='password' id='password1' name='password1' style='width:10em' value='<?= $_POST['password1'] ?>' /></td></tr>
 		<tr class='comp <?= $post && $bad['password2'] ? "bad" : "" ?>'><td><label for='password2'>Confirm Password:</label></td><td><input type='password' id='password2' name='password2' style='width:10em' value='<?= $_POST['password2'] ?>' /></td></tr>
@@ -47,6 +58,7 @@ else
 	require_once('/home/opendatamap/mysql.inc.php');
 	$params = array();
 	$params[] = "'".mysql_real_escape_string($_POST['username'])."'";
+	$params[] = "'".mysql_real_escape_string($_POST['email'])."'";
 	$params[] = "'".md5($_POST['password1'])."'";
 	$q = "INSERT INTO users VALUES (".implode(',', $params).")";
 	$res = mysql_query($q);
