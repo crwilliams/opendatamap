4e75effb43c24458a6f7ce183b03841da8a734d9
Colin Williams <github@crwilliams.co.uk>
2011-11-26 22:05:05 +0000

    Added email field
    git-svn-id: svn://svn.forge.ecs.soton.ac.uk/projects/opendatamap@289 74a4a7be-f69d-0410-a7fa-b81c2d4ed421

--- www/htdocs/mymap/login.php
+++ www/htdocs/mymap/login.php
@@ -1,12 +1,16 @@
 <?
 error_reporting(0);
 session_start();
+$error = "";
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	require_once('/home/opendatamap/mysql.inc.php');
 	$params[] = mysql_real_escape_string($_POST['username']);
 	$params[] = md5($_POST['password']);
-	$q = 'SELECT * FROM users WHERE username = \''.$params[0].'\' AND password = \''.$params[1].'\'';
+	if(strpos($_POST['username'], '@') !== 0)
+		$q = 'SELECT * FROM users WHERE email = \''.$params[0].'\' AND password = \''.$params[1].'\'';
+	else
+		$q = 'SELECT * FROM users WHERE username = \''.$params[0].'\' AND password = \''.$params[1].'\'';
 	$res = mysql_query($q);
 	$row = mysql_fetch_assoc($res);
 	if($row)
@@ -19,7 +23,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	}
 	else
 	{
-		echo 'Incorrect credentials, please try again';
+		$error = 'Incorrect credentials, please try again';
 	}
 }
 if(isset($_SESSION['username']))
@@ -32,10 +36,14 @@ if(isset($_SESSION['username']))
 }
 include 'functions.inc.php';
 outputHeader("Log in", "", "GENERIC", true, false);
+if($error != "")
+{
+	echo '<div style="background-color:#FF9999; margin:10px; padding:10px; text-align:center">'.$error.'</div>';
+}
 ?>
 <form action='login' method='post'>
 	<table>
-		<tr class='comp'><td><label for='username'>Username:</label></td><td><input id='username' name='username' style='width:10em' /></td></tr>
+		<tr class='comp'><td><label for='username'>Username or E-mail Address:</label></td><td><input id='username' name='username' style='width:10em' /></td></tr>
 		<tr class='comp'><td><label for='password'>Password:</label></td><td><input type='password' id='password' name='password' style='width:10em' /></td></tr>
 		<tr><td /><td><input type='submit' /></td></tr>
 	</table>
