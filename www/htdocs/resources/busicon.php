599345da8c9686336addb032852fa20706db7a63
Colin Williams <github@crwilliams.co.uk>
2013-01-12 20:36:34 +0000

    Added U1A, U1C, U1E, U2B, U2C, U6C and U6H.

--- www/htdocs/resources/busicon.php
+++ www/htdocs/resources/busicon.php
@@ -7,8 +7,15 @@ if(!file_exists($filename))
 	$oimg = imagecreatefrompng("../img/icon/Transportation/bus.png");
 	$white = imagecolorallocate($oimg, 255, 255, 255);
 	$color['U1'] = imagecolorallocate($oimg,   0, 142, 207);
+	$color['U1A'] = $color['U1'];
+	$color['U1C'] = $color['U1'];
+	$color['U1E'] = $color['U1'];
 	$color['U2'] = imagecolorallocate($oimg, 226,   2,  20);
+	$color['U2B'] = $color['U2'];
+	$color['U2C'] = $color['U2'];
 	$color['U6'] = imagecolorallocate($oimg, 246, 166,  24);
+	$color['U6C'] = $color['U6'];
+	$color['U6H'] = $color['U6'];
 	$color['U9'] = imagecolorallocate($oimg, 232,  84, 147);
 	$i = 0;
 	foreach($rs as $r)
