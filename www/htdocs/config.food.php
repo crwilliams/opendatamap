926039fb0e49e85ad2b3440a779fc0875b1fe35c
Colin Williams <github@crwilliams.co.uk>
2013-01-20 00:45:18 +0000

    Added map versions.

--- www/htdocs/config.food.php
+++ www/htdocs/config.food.php
@@ -13,4 +13,24 @@ for($i = 0; $i <= 5; $i++)
 {
 	$config['categories']['fhrs_'.$i.'_en-gb'] = 'Food Hygiene Rating: '.$i;
 }
+
+foreach(glob('/home/opendatamap/FHRS/*.xml') as $version)
+{
+	$version = str_replace(array('/home/opendatamap/FHRS/', '.xml'), '', $version);
+	$placename = str_replace('_', ' ', $version);
+	$config['versions'][$version]['datafile'] = $version;
+	$config['versions'][$version]['Site title'] = "Food Hygiene Map for ".$placename;
+	$config['versions'][$version]['Site subtitle'] = $placename;
+	$config['versions'][$version]['Site description'] = "Interactive map showing food establishments in ".$placename." and their hygiene ratings";
+	$config['versions'][$version]['Site keywords'] = "Food Hygiene,map,interactive,".$placename;
+	$config['versions'][$version]['hidden'] = true;
+}
+
+if(isset($config['versions'][$versionparts[1]]))
+{
+	foreach($config['versions'][$versionparts[1]] as $key => $value)
+	{
+		$config[$key] = $value;
+	}
+}
 ?>
