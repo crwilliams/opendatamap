ae02a4d1c367db6199caa8f24e33488013e638ea
Colin Williams <github@crwilliams.co.uk>
2013-01-20 11:04:52 +0000

    Corrected datasource name.

--- www/htdocs/modules/southampton/config.php
+++ www/htdocs/modules/southampton/config.php
@@ -5,7 +5,7 @@ $config['Site description'] = "Interactive Map of the University of Southampton,
 $config['default lat'] = 50.9355;
 $config['default long'] = -1.39595;
 $config['default zoom'] = 17;
-$config['datasource'] = array('southamptoncached', 'postcode', 'sucu', /*'oxford', 'cambridge'*/);
+$config['datasource'] = array('southampton', 'postcode', 'sucu', /*'oxford', 'cambridge'*/);
 $config['prefix'] = "http://id.southampton.ac.uk/";
 
 $config['versions']['embed']['Site title'] = "embed";
@@ -14,12 +14,12 @@ $config['versions']['embed']['hidden'] = true;
 
 $config['versions']['catering']['Site title'] = "Catering map";
 $config['versions']['catering']['enabled'] = array('search', 'geobutton', '-title');
-$config['versions']['catering']['datasource'] = array('southamptoncached', 'postcode');
+$config['versions']['catering']['datasource'] = array('southampton', 'postcode');
 $config['versions']['catering']['unused-datasets'] = array('http://id.southampton.ac.uk/dataset/bus-routes', 'http://id.southampton.ac.uk/dataset/bus-stops', 'http://id.southampton.ac.uk/dataset/room-features', 'http://id.southampton.ac.uk/dataset/wifi', 'http://data.ordnancesurvey.co.uk');
 
 $config['versions']['isolutions-wifi']['Site title'] = "iSolutions WiFi map";
 $config['versions']['isolutions-wifi']['enabled'] = array('geobutton', '-title');
-$config['versions']['isolutions-wifi']['datasource'] = array('southamptoncached');
+$config['versions']['isolutions-wifi']['datasource'] = array('southampton');
 $config['versions']['isolutions-wifi']['unused-datasets'] = array('http://id.southampton.ac.uk/dataset/amenities', 'http://id.southampton.ac.uk/dataset/bus-routes', 'http://id.southampton.ac.uk/dataset/bus-stops', 'http://id.southampton.ac.uk/dataset/room-features', 'http://id.southampton.ac.uk/dataset/catering', 'http://data.ordnancesurvey.co.uk');
 
 if(isset($config['versions'][$versionparts[1]]))
