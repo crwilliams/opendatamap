01c6f4e0d046bb81613b01f94706a53391d470e5
Colin Williams <github@crwilliams.co.uk>
2013-01-20 12:17:58 +0000

    Configure icons to be generated for 'food' module.

---
+++ www/htdocs/modules/food/icons.php
@@ -0,0 +1,25 @@
+<?php
+$cols['fhrs_0_en-gb'] = 'c03638';
+$cols['fhrs_1_en-gb'] = 'f34648';
+$cols['fhrs_2_en-gb'] = 'ff8a22';
+$cols['fhrs_3_en-gb'] = 'ffc11f';
+$cols['fhrs_4_en-gb'] = '66c547';
+$cols['fhrs_5_en-gb'] = '128e4d';
+for($rating = 0; $rating <= 5; $rating++)
+{
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'bar_coktail.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'conveniencestore.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'factory.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'family.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'farm-2.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'foodtruck.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'fruits.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'lodging_0star.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'restaurant.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'school.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'supermarket.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'takeaway.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'teahouse.png';
+	$icons['fhrs_'.$rating.'_en-gb'][] = 'truck3.png';
+}
+?>
