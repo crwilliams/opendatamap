fec0fa1f4bb5d12be773e31df5a22a148591e341
Colin Williams <github@crwilliams.co.uk>
2013-01-27 00:21:14 +0000

    Removed commented out code.

--- www/htdocs/modules/openday/config.php
+++ www/htdocs/modules/openday/config.php
@@ -1,39 +1,11 @@
 <?
-/*
-switch($versionparts[1])
-{
-	case 'friday':
-		$date = ' | Friday 8th July 2011';
-		break;
-	case 'saturday':
-		$date = ' | Saturday 9th July 2011';
-		break;
-}
-
-$file = fopen('resources/opendaycourses.csv', 'r');
-while($data = fgetcsv($file))
-{
-	if($data[0] == $versionparts[2])
-	{
-		$course = $data[1];
-		break;
-	}
-}
-fclose($file);
-*/
-
 $config['Site title'] = "University of Southampton Open Day Map";
-//if($course)
-//	$config['Site title'] .= " | $course";
 $config['Site keywords'] = "University of Southampton,open day,map,Southampton,amenity,bus stop,building,site,campus,interactive";
 $config['Site description'] = "Interactive Open Day Map of the University of Southampton";
 $config['default lat'] = 50.9355;
 $config['default long'] = -1.39595;
 $config['default zoom'] = 13;
-//$config['default map'] = "google.maps.MapTypeId.SATELLITE";
 $config['datasource'] = array('openday', 'postcode', /*'oxford', 'cambridge'*/);
-//if(isset($versionparts[2]) && isset($versionparts[3])
-//	$q = $versionparts[2].'/'.$;
 if($versionparts[1] == 'iframe')
 {
 	$config['enabled'] = array('sidebarhidden', 'bookmarks', '-title');
