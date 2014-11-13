439f6518a28056b947cdf49be09ac829109c6770
Colin Williams <github@crwilliams.co.uk>
2014-11-13 19:26:55 +0000

    Fix bus times info window.

--- www/htdocs/bus.php
+++ www/htdocs/bus.php
@@ -3,6 +3,12 @@ $uri = urldecode($_GET['uri']);
 if(preg_match('/http:\/\/id\.southampton\.ac\.uk\/bus-stop\/(.*)/', $uri, $matches))
 {
 	echo "<style>table{width:100%; margin:3px} td{background-color:black; color:yellow; margin:3px; padding:3px}</style>";
-	fpassthru(fopen("http://data.southampton.ac.uk/bus-stop/".$matches[1].".html?view=embed", 'r'));
+        $data = json_decode(file_get_contents('http://data.southampton.ac.uk/bus-stop/'.$matches[1].'.json?max=6', 'r'));
+	echo '<table>';
+	foreach($data->stops as $stop)
+	{
+		echo '<tr><td>'.$stop->name.'</td><td>'.$stop->dest.'</td><td>'.$stop->time.'</td></tr>';
+	}
+	echo '</table>';
 }
 ?>
